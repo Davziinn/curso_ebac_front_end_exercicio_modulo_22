974332738cdb43173f3e4ef007e3edd2e38d8230
Davi <mdavi6085@gmail.com>
2024-08-20 09:37:19 -0300

    finalizando o projeto

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -584,7 +584,25 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
-console.log("OL\xc1 MUNDO!");
+const dataEvento = new Date("April 12, 2025 21:00:00");
+const timeStampEvento = dataEvento.getTime();
+const contaHora = setInterval(()=>{
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteEvento = timeStampEvento - timeStampAtual;
+    const diaEmMS = 86400000;
+    const horasEmMS = 3600000;
+    const minutosEmMS = 60000;
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMS);
+    const horasAteEvento = Math.floor(distanciaAteEvento % diaEmMS / horasEmMS);
+    const minutosAteEvento = Math.floor(distanciaAteEvento % horasEmMS / minutosEmMS);
+    const segundosAteEvento = Math.floor(distanciaAteEvento % minutosEmMS / 1000);
+    document.getElementById("contador").innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaHora);
+        document.getElementById("contador").innerHTML = "EVENTO J\xc1 ENCERRADO!!!";
+    }
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequired256")
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('OLÁ MUNDO!')
+const dataEvento = new Date('April 12, 2025 21:00:00');
+const timeStampEvento = dataEvento.getTime();
+
+const contaHora = setInterval(() =>{
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampEvento - timeStampAtual;
+
+    const diaEmMS = 1000 * 60 * 60 * 24;
+    const horasEmMS = 1000 * 60 * 60;
+    const minutosEmMS = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMS);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMS) / horasEmMS);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horasEmMS) / minutosEmMS);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutosEmMS) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`
+
+    if(distanciaAteEvento < 0){
+        clearInterval(contaHora);
+
+        document.getElementById('contador').innerHTML = 'EVENTO JÁ ENCERRADO!!!'
+    }
+
+}, 1000);
